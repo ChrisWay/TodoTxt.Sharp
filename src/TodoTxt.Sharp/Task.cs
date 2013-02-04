2fabe73d08bbf6b64f1a82d74afd5e1f0a413205
ChrisWay <chris@darksquare.net>
2013-02-04 19:02:13 +0000

    Added basic Task model.

---
+++ src/TodoTxt.Sharp/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoTxt.Sharp
+{
+	public class Task
+	{
+		public string Content { get; set; }
+
+		public bool HasPriority
+		{
+			get { return Priority.HasValue; }
+		}
+
+		public char? Priority { get; set; }
+
+		public bool IsCompleted { get; set; }
+		public DateTime? CompletionDate { get; set; }
+		
+		public DateTime? CreationDate { get; set; }
+
+		public IList<string> Contexts { get; set; }
+		public IList<string> Projects { get; set; }
+
+		public string Raw { get; set; }
+	}
+}
