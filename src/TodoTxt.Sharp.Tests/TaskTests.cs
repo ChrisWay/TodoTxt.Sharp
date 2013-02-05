cc645dfd226a9773d0e1e1a239741c6ecd2e3afe
Chris Way <chris@darksquare.net>
2013-02-05 09:21:04 +0000

    Added more Priority Tests.

--- src/TodoTxt.Sharp.Tests/TaskTests.cs
+++ src/TodoTxt.Sharp.Tests/TaskTests.cs
@@ -11,12 +11,39 @@ namespace TodoTxt.Sharp.Tests
     public class TaskTests
     {
         [Test]
-        public void Priority_ATaskWithAPriority_PriorityPropertySet()
+        public void Priority_TaskWithAPriority_IsA()
         {
             var t = new Task("(A) a task");
             
             Assert.AreEqual('A', t.Priority);
             Assert.IsTrue(t.HasPriority);
+        }
+
+        [Test]
+        public void Priority_TaskWithALowerCasePriority_IsNull()
+        {
+            var t = new Task("(a) a task");
+
+            Assert.IsNull(t.Priority);
+            Assert.IsFalse(t.HasPriority); 
+        }
+
+        [Test]
+        public void Priority_TaskWithTwoPriorities_IsA()
+        {
+            var t = new Task("(A)(B) a task");
+            
+            Assert.AreEqual('A', t.Priority);
+            Assert.IsTrue(t.HasPriority);
+        }
+
+        [Test]
+        public void Priority_TaskWithNoPriority_IsNull()
+        {
+            var t = new Task("a task");
+
+            Assert.IsNull(t.Priority);
+            Assert.IsFalse(t.HasPriority);
         }
     }
 }
