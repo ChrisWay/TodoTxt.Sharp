64f47fa2c153cd097af83718db91f30dc034d945
ChrisWay <chris@darksquare.net>
2014-01-16 18:12:31 +0000

    VERY basic test UI
    Note: Doesn't really do anything!

--- src/TodoTxt.Sharp.UI/MainWindow.xaml.cs
+++ src/TodoTxt.Sharp.UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace TodoTxt.Sharp.UI
 {
@@ -20,9 +22,30 @@ namespace TodoTxt.Sharp.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TaskFile _file;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fileChooser = new OpenFileDialog();
+
+            if (fileChooser.ShowDialog() == true) {
+                _file = new TaskFile(fileChooser.FileName);
+                TaskListBox.ItemsSource = _file.Tasks;
+            }
+        }
+
+        private void InputTextBox_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key != Key.Return)
+                return;
+
+            _file.AddTask(new Task(InputTextBox.Text));
+            InputTextBox.Clear();
+
         }
     }
 }
